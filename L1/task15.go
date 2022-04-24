728f4ca1328f0e4b522982bcb75eded6758573ae
sQUARys <46746212+sQUARys@users.noreply.github.com>
2022-04-24 21:37:52 +0300

    Task #15: Status - DONE

--- L1/task15.go
+++ L1/task15.go
@@ -1,39 +1,32 @@
 package main
 
-//К каким негативным последствиям может привести данный фрагмент кода, и как это исправить?
-//Приведите корректный пример реализации.
+import "fmt"
+
+//К каким негативным последствиям может привести данный фрагмент кода, и как это исправить? Приведите корректный пример реализации.
 //
 //var justString string
-
-//
 //func someFunc() {
 //	v := createHugeString(1 << 10)
 //	justString = v[:100]
 //}
-
+//
 //func main() {
-//	justString := "HiHowatryou"
-//	//someFunc()
-//	justString[:5]
+//	someFunc()
 //}
 
-import "fmt"
+var justString string
 
-var c string
+func someFunc() {
+	v := createHugeString(1 << 10) //создаем строку длинной 1024
+	justString = v[:100]           // записываем срез в переменную
+
+	ArrCopy := make([]byte, len(justString)) // создаем массив байт для копирования в него среза
+
+	copy(ArrCopy, justString) // копируем срез, чтобы на v больше ни один срез не ссылался. Тем самым v не будет больше хранится в памяти
+
+	fmt.Println(ArrCopy)
+}
 
 func main() {
-	a := "12345"
-	//c := copyString(a[:2])
-	c := a[:3]
-	fmt.Println(c, a)
+	someFunc()
 }
-
-func copyString(b string) string {
-	c := make([]byte, len(b))
-
-	copy(c, b)
-
-	return string(c)
-}
-
-// ??????
