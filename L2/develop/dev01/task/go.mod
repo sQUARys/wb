3040595250d6100e61a238fa9aab77562018c4b9
Roman <roman.kocenko.2004@mai.ru>
2022-05-01 12:20:41 +0300

    Dev03: TODO: tests and func SortByNumber.

---
+++ L2/develop/dev01/task/go.mod
@@ -0,0 +1,9 @@
+module package/task
+
+go 1.18
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
--- L2/develop/dev03/task.go
+++ L2/develop/dev03/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 /*
 === Утилита sort ===
 
@@ -8,10 +13,10 @@ package main
 
 Поддержать ключи
 
--k — указание колонки для сортировки
--n — сортировать по числовому значению
--r — сортировать в обратном порядке
--u — не выводить повторяющиеся строки
+-k — указание колонки для сортировки Done
+-n — сортировать по числовому значению ??
+-r — сортировать в обратном порядке Done
+-u — не выводить повторяющиеся строки Done
 
 Дополнительное
 
@@ -25,6 +30,55 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+func Reverse(sl []string) []string {
+	sort.Strings(sl)
+	sort.Sort(sort.Reverse(sort.StringSlice(sl)))
+	return sl
+}
+
+func SortWithoutRepeat(sl []string) []string {
+	var mem []string
+	sort.Strings(sl)
+	for i := range sl {
+		if len(mem) == sort.SearchStrings(mem, sl[i]) { // если не найдено такой строки
+			mem = append(mem, sl[i])
+		}
+	}
+	return mem
+}
+
+func SortBySpecialColumn(sl []string, k int) []string {
+	sort.Slice(sl, func(i, j int) bool {
+		return sl[i][k] < sl[j][k]
+	})
+	return sl
+}
+
+func SortByNumber(sl []string) {
+	//?????
+}
+
 func main() {
+	var choice string
+	str := []string{"abc", "acs", "bfd", "aaa", "aaa", "bbb"}
+
+	fmt.Scanf("%s\n", &choice)
+
+	switch choice {
+	case "k":
+		var k int
+		fmt.Println("Вы выбрали отсортировать по определенной колонке.")
+		fmt.Print("Введите индекс колонки:")
+		fmt.Scanf("%d\n", &k)
+		fmt.Println(SortBySpecialColumn(str, k))
+	case "r":
+		fmt.Println("Вы выбрали отсортировать в обратном порядке.")
+		fmt.Println(Reverse(str))
+	case "u":
+		fmt.Println("Вы выбрали отсортировать и не выводить повторяющиеся строки.")
+		fmt.Println(SortWithoutRepeat(str))
+	default:
+		fmt.Println("Вы ввели несуществующую команду")
+	}
 
 }
