c64c9f71131daf2d12406ad7dc0392cf845507e4
Roman <roman.kocenko.2004@mai.ru>
2022-05-09 17:57:27 +0300

    Dev06: Done fields and delimiter(tab in progress)

--- L2/develop/dev06/task.go
+++ L2/develop/dev06/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -14,5 +24,47 @@ package main
 */
 
 func main() {
+	configFile, err := ioutil.ReadFile("data.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	var configLines []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
+	if len(text) == 0 {
+		fmt.Println("Ошибка ввода.")
+		return
+	}
+
+	arr := strings.Split(text, "-")
+
+	for i := 1; i < len(arr); i++ {
+
+		if strings.Contains(arr[i], "d") {
+			dVal := strings.Index(arr[i], "'")
+			//fmt.Println(string(arr[i][val]), "he")
+			delimiter := strings.Split(arr[i][dVal:], "'")
+			fmt.Println(delimiter)
+			delimiter = delimiter[1 : len(delimiter)-1]
+			fmt.Println(delimiter[0])
+			//как разделить по табу
+			configLines = strings.Split(string(configFile), delimiter[0]) // разделяем каждое слово строки на элементы массива
+			for j := range configLines {
+				fmt.Println(configLines[j])
+			}
+		}
+		if strings.Contains(arr[i], "f") {
+			var fields []string
+			fmt.Println(arr[i])
+			field, _ := strconv.Atoi(strings.Fields(arr[i])[1])
+			for f := range configLines {
+				fields = strings.Fields(configLines[f])
+				fmt.Println(fields[field-1])
+			}
+		}
+	}
 
 }
