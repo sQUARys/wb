c4e61f709f1ad1deaa9ec07a4e40f916623a6b04
Roman <roman.kocenko.2004@mai.ru>
2022-05-09 20:18:48 +0300

    Dev09: Try of wget

--- L2/develop/dev09/task.go
+++ L2/develop/dev09/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +18,52 @@ package main
 */
 
 func main() {
+
+	fmt.Println("\nDownloading file...\n")
+
+	fileUrl, err := url.Parse(*cliUrl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	filePath := fileUrl.Path
+	segments := strings.Split(filePath, "/")
+	fileName := segments[len(segments)-1]
+	file, err := os.Create(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer file.Close()
+
+	checkStatus := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			r.URL.Opaque = r.URL.Path
+			return nil
+		},
+	}
+
+	response, err := checkStatus.Get(*cliUrl)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer response.Body.Close()
+	fmt.Printf("Request Status: %s\n\n", response.Status)
+
+	filesize := response.ContentLength
+
+	go func() {
+		n, err := io.Copy(file, response.Body)
+		if n != filesize {
+			fmt.Println("Truncated")
+		}
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+	}()
 
 }
