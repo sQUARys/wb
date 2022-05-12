944e59faed3556d4e265a044970266b9a63e2362
Roman <roman.kocenko.2004@mai.ru>
2022-05-12 11:25:49 +0300

    Dev07: Delete unused

--- L2/develop/dev07/task.go
+++ L2/develop/dev07/task.go
@@ -37,36 +37,6 @@ start := time.Now()
 
 fmt.Printf(“fone after %v”, time.Since(start))
 */
-
-func or1(channels ...<-chan interface{}) <-chan interface{} {
-	switch len(channels) {
-	case 0:
-		return nil
-	case 1:
-		return channels[0]
-	}
-
-	orDone := make(chan interface{})
-	go func() {
-		defer close(orDone)
-		switch len(channels) {
-		case 2:
-			select {
-			case <-channels[0]:
-			case <-channels[1]:
-			}
-		default:
-			select {
-			case <-channels[0]:
-			case <-channels[1]:
-			case <-channels[2]:
-			case <-or(append(channels[3:], orDone)...):
-			}
-		}
-	}()
-	return orDone
-}
-
 func or(channels ...<-chan interface{}) <-chan interface{} {
 	doneCh := make(chan interface{})
 
@@ -80,7 +50,7 @@ func or(channels ...<-chan interface{}) <-chan interface{} {
 			defer close(doneCh)
 			select {
 			case <-channels[0]:
-			case <-or(append(channels[1:], doneCh)...):
+			case <-or(append(channels[1:])...):
 			}
 		}(doneCh)
 	}
