c198c7e372810a982d02be30d696e0f66806cba5
Roman <roman.kocenko.2004@mai.ru>
2022-05-12 11:42:17 +0300

    Dev01: Modules try

--- L2/develop/dev01/main.go
+++ L2/develop/dev01/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/sQUARys/wb/L2/develop/dev01/module"
 )
 
 func main() {
-	message := task.GetTime()
+	message := module.GetTime()
 	fmt.Println(message)
 }
--- L2/develop/dev01/module/go.mod
+++ L2/develop/dev01/module/go.mod
@@ -1,4 +1,4 @@
-module mod
+module github.com/sQUARys/wb/L2/develop/dev01/module
 
 go 1.18
 
