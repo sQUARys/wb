41077791e5c4a63fb7a68bd1580968d3498abaa7
Roman <roman.kocenko.2004@mai.ru>
2022-05-18 13:55:10 +0300

    Exam: flags working

--- L2/exam.go
+++ L2/exam.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net"
+	"flag"
 )
 
-type Server struct {
-	connection        net.Conn
-	errorOfConnection error
-	connectToSite     string
-}
+var (
+	timeout       string
+	pathToConnect string
+)
 
-func (srv *Server) hasConnection() {
-	str := "Hi"
-	srv.connectToSite = str
+func flagSet() {
+	flag.StringVar(&timeout, "timeout", "default", "A timeout Var")
+	flag.Parse()
+	args := flag.Args()
+	pathToConnect = args[0] + ":" + args[1]
 }
 
 func main() {
-	err := io.EOF
-	server := Server{
-		errorOfConnection: err,
-	}
-	fmt.Println(server)
-	server.hasConnection()
-	fmt.Println(server)
+	flagSet()
+
 }
