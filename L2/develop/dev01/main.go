72e4d88b713e618941a476bce63bd855f0004a74
Roman <roman.kocenko.2004@mai.ru>
2022-05-20 11:15:20 +0300

    DEV01: question. Error in main.go

--- L2/develop/dev01/main.go
+++ L2/develop/dev01/main.go
@@ -6,6 +6,12 @@ import (
 )
 
 //Can't find go-modules. Why?
+//Error
+//go: finding module for package github.com/sQUARys/Go-Modules/task
+//go: downloading github.com/sQUARys/Go-Modules v0.0.0-20220512093447-2844b6347ec0
+//mod imports
+//github.com/sQUARys/Go-Modules/task: module github.com/sQUARys/Go-Modules@latest found (v0.0.0-20220512093447-2844b6347ec0), but does not contain package github.com/sQUARys/Go-Modules/task
+
 func main() {
 	message := task.GetTime()
 	fmt.Println(message)
