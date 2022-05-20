c355ebda3b9669fbb801575c5a0cbbfb97390358
Roman <roman.kocenko.2004@mai.ru>
2022-05-20 11:06:15 +0300

    DEV06: TODO: do all in func, tests

--- L2/develop/dev06/task.go
+++ L2/develop/dev06/task.go
@@ -23,6 +23,33 @@ import (
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+func Delimit(i int, arr []string, configFile []byte) (bool, string, []string) {
+	fmt.Println("Delimiter...")
+	dVal := strings.Index(arr[i], "'")
+	delimiter := strings.Split(arr[i][dVal:], "'")
+	delimiter = delimiter[1 : len(delimiter)-1]
+	configLines := strings.Split(string(configFile), delimiter[0]) // разделяем каждое слово строки на элементы массива
+	isDelimited := true
+	memDel := delimiter[0]
+	return isDelimited, memDel, configLines
+}
+
+//Переделать!
+func Fields(i int, arr []string, configLines []string) bool {
+	fmt.Println("Fields...")
+	var fields []string
+	field, _ := strconv.Atoi(strings.Fields(arr[i])[1])
+
+	if field >= len(configLines) {
+		fmt.Println("Вы ввели поле, которое выходит за границы строк. Введите пожалуйста значение поменьше")
+		return
+	}
+	for f := range configLines {
+		fields = strings.Fields(configLines[f])
+		fmt.Println(fields[field-1])
+	}
+}
+
 func main() {
 	var memDel string
 	isDelimited := false
@@ -46,16 +73,7 @@ func main() {
 
 	for i := 1; i < len(arr); i++ {
 		if strings.Contains(arr[i], "d") {
-			fmt.Println("Delimiter...")
-			dVal := strings.Index(arr[i], "'")
-			delimiter := strings.Split(arr[i][dVal:], "'")
-			delimiter = delimiter[1 : len(delimiter)-1]
-			configLines = strings.Split(string(configFile), delimiter[0]) // разделяем каждое слово строки на элементы массива
-			for j := range configLines {
-				fmt.Println(configLines[j])
-			}
-			isDelimited = true
-			memDel = delimiter[0]
+			isDelimited, memDel, configLines = Delimit(i, arr, configFile)
 		}
 		if strings.Contains(arr[i], "f") {
 			if !isDelimited {
@@ -63,20 +81,7 @@ func main() {
 				configLines = strings.Split(string(configFile), "\t") // разделяем каждое слово строки на элементы массива
 				fmt.Println(configLines[0])
 			}
-
-			fmt.Println("Fields...")
-			var fields []string
-			field, _ := strconv.Atoi(strings.Fields(arr[i])[1])
-
-			if field >= len(configLines) {
-				fmt.Println("Вы ввели поле, которое выходит за границы строк. Введите пожалуйста значение поменьше")
-				return
-			}
-			for f := range configLines {
-				fields = strings.Fields(configLines[f])
-				fmt.Println(fields[field-1])
-			}
-
+			Fields(i, arr, configLines)
 		}
 		if strings.Contains(arr[i], "s") {
 			fmt.Println("Only-delimited...")
