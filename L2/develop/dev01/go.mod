82bc2e862b3692b5769b32a2a3d38faf3a44e41c
Roman <roman.kocenko.2004@mai.ru>
2022-05-23 17:15:27 +0300

    DEV01: DONE

--- L2/develop/dev01/go.mod
+++ L2/develop/dev01/go.mod
@@ -1,3 +1,14 @@
 module mod
 
 go 1.18
+
+require github.com/sQUARys/Go-Modules v0.0.0-20220523140826-0794ca832220
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
--- L2/develop/dev01/main.go
+++ L2/develop/dev01/main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/sQUARys/Go-Modules/task"
+	task "github.com/sQUARys/Go-Modules"
 )
-
-//Can't find go-modules. Why?
-//Error
-//go: finding module for package github.com/sQUARys/Go-Modules/task
-//go: downloading github.com/sQUARys/Go-Modules v0.0.0-20220512093447-2844b6347ec0
-//mod imports
-//github.com/sQUARys/Go-Modules/task: module github.com/sQUARys/Go-Modules@latest found (v0.0.0-20220512093447-2844b6347ec0), but does not contain package github.com/sQUARys/Go-Modules/task
 
 func main() {
 	message := task.GetTime()
