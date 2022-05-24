8fb3963a2b4b3aff0136edb15daac9e535aaa44e
Roman <roman.kocenko.2004@mai.ru>
2022-05-24 15:06:58 +0300

    DEV11: start

--- L2/develop/dev11/task.go
+++ L2/develop/dev11/task.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"regexp"
+	"sync"
 )
 
 /*
@@ -17,7 +18,7 @@ import (
 	4. Реализовать middleware для логирования запросов
 Методы API: POST /create_event POST /update_event POST /delete_event GET /events_for_day GET /events_for_week GET /events_for_month
 Параметры передаются в виде www-url-form-encoded (т.е. обычные user_id=3&date=2019-09-09).
-В GET методах параметры передаются через queryString, в POST через тело запроса.
+В GET методах параметры передаются через querystring, в POST через тело запроса.
 В результате каждого запроса должен возвращаться JSON документ содержащий либо {"result": "..."} в случае успешного выполнения метода,
 либо {"error": "..."} в случае ошибки бизнес-логики.
 
@@ -28,48 +29,184 @@ import (
 	4. Код должен проходить проверки go vet и golint.
 */
 
-//func Serialize(obj interface{}) (string, error) {
-//	data, err := json.Marshal(obj)
+// renderJSON преобразует 'v' в формат JSON и записывает результат, в виде ответа, в w.
+//func renderJSON(w http.ResponseWriter, v interface{}) {
+//	js, err := json.Marshal(v)
 //	if err != nil {
-//		return "", err
-//	} else {
-//		return string(data), err
+//		http.Error(w, err.Error(), http.StatusInternalServerError)
+//		return
 //	}
+//	w.Header().Set("Content-Type", "application/json")
+//	w.Write(js)
 //}
+//
+//func main() {
+//	mux := http.NewServeMux()
+//
+//	mux.HandleFunc("/task/", taskHandler())
+//}
+//package main
 
-func RequestMethods(w http.ResponseWriter, r *http.Request) {
-	log.Println("%s %s %s\n", r.Method, r.URL, r.Proto)
+var (
+	//POST
+	createEvent = regexp.MustCompile(`^\/create_event[\/]?.+$`) // ^$ - полное совпадение должно быть
+	updateEvent = regexp.MustCompile(`^\/update_event[\/]?.+$`) // \d+ - одна или более цифра доступная позже
+	deleteEvent = regexp.MustCompile(`^\/delete_event[\/]?.+$`) // * - жабная функция
+	//GET
+	getEventForDay   = regexp.MustCompile(`^\/events_for_day[\/]$`)
+	getEventForWeek  = regexp.MustCompile(`^\/events_for_week[\/]$`)
+	getEventForMonth = regexp.MustCompile(`^\/events_for_month[\/]$`)
+)
 
-	if r.URL.Path != "/" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type datastore struct {
+	m map[string]user
+	*sync.RWMutex
+}
+
+type userHandler struct {
+	store *datastore
+}
+
+func (h *userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	////POST
+	//createEvent = regexp.MustCompile(`^\/create_event[\/]?.+$`) // ^$ - полное совпадение должно быть
+	//updateEvent = regexp.MustCompile(`^\/update_event[\/]?.+$`) // \d+ - одна или более цифра доступная позже
+	//deleteEvent = regexp.MustCompile(`^\/delete_event[\/]?.+$`) // * - жабная функция
+	////GET
+	//getEventForDay   = regexp.MustCompile(`^\/events_for_day[\/]$`)
+	//getEventForWeek  = regexp.MustCompile(`^\/events_for_week[\/]$`)
+	//getEventForMonth = regexp.MustCompile(`^\/events_for_month[\/]$`)
+
+	w.Header().Set("content-type", "application/json")
+	r.ParseForm()
+	queryMap := r.Form
+	fmt.Println(queryMap, r.Method, createEvent.MatchString(r.URL.Path))
+	switch {
+	case r.Method == http.MethodGet && getEventForDay.MatchString(r.URL.Path):
+		fmt.Println("Get1", queryMap)
+		//h.List(w, r)
 		return
-	}
-
-	switch r.Method {
-	case "GET":
-		http.ServeFile(w, r, "form.html")
-	case "POST":
-		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
-		if err := r.ParseForm(); err != nil {
-			fmt.Fprintf(w, "ParseForm() err: %v", err)
-			return
-		}
-		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
-		name := r.FormValue("name")
-		address := r.FormValue("address")
-		fmt.Fprintf(w, "Name = %s\n", name)
-		fmt.Fprintf(w, "Address = %s\n", address)
-
+	case r.Method == http.MethodGet && getEventForWeek.MatchString(r.URL.Path):
+		fmt.Println("Get2", queryMap)
+		//h.Get(w, r)
+		return
+	case r.Method == http.MethodGet && getEventForMonth.MatchString(r.URL.Path):
+		fmt.Println("Get3", queryMap)
+		//h.List(w, r)
+		return
+	case r.Method == http.MethodPost && createEvent.MatchString(r.URL.Path):
+		fmt.Println("Post1", queryMap)
+		//h.Create(w, r)
+		return
+	case r.Method == http.MethodPost && updateEvent.MatchString(r.URL.Path):
+		fmt.Println("Post2", queryMap)
+		//h.Create(w, r)
+		return
+	case r.Method == http.MethodPost && deleteEvent.MatchString(r.URL.Path):
+		fmt.Println("Post3", queryMap)
+		//h.Create(w, r)
+		return
 	default:
-		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+		//notFound(w, r)
+		return
 	}
 }
 
-func main() {
-	http.HandleFunc("/", RequestMethods)
+//func (h *userHandler) List(w http.ResponseWriter, r *http.Request) {
+//	h.store.RLock()
+//	users := make([]user, 0, len(h.store.m))
+//	for _, v := range h.store.m {
+//		users = append(users, v)
+//	}
+//	h.store.RUnlock()
+//	jsonBytes, err := json.Marshal(users)
+//	if err != nil {
+//		internalServerError(w, r)
+//		return
+//	}
+//	w.WriteHeader(http.StatusOK)
+//	w.Write(jsonBytes)
+//}
+//
+//func (h *userHandler) Get(w http.ResponseWriter, r *http.Request) {
+//	matches := getUserRe.FindStringSubmatch(r.URL.Path)
+//	if len(matches) < 2 {
+//		notFound(w, r)
+//		return
+//	}
+//	h.store.RLock()
+//	u, ok := h.store.m[matches[1]]
+//	h.store.RUnlock()
+//	if !ok {
+//		w.WriteHeader(http.StatusNotFound)
+//		w.Write([]byte("user not found"))
+//		return
+//	}
+//	jsonBytes, err := json.Marshal(u)
+//	if err != nil {
+//		internalServerError(w, r)
+//		return
+//	}
+//	w.WriteHeader(http.StatusOK)
+//	w.Write(jsonBytes)
+//}
+//
+//func (h *userHandler) Create(w http.ResponseWriter, r *http.Request) {
+//	var u user
+//	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+//		internalServerError(w, r)
+//		return
+//	}
+//	h.store.Lock()
+//	h.store.m[u.ID] = u
+//	h.store.Unlock()
+//	jsonBytes, err := json.Marshal(u)
+//	if err != nil {
+//		internalServerError(w, r)
+//		return
+//	}
+//	w.WriteHeader(http.StatusOK)
+//	w.Write(jsonBytes)
+//}
+//
+//func internalServerError(w http.ResponseWriter, r *http.Request) {
+//	w.WriteHeader(http.StatusInternalServerError)
+//	w.Write([]byte("internal server error"))
+//}
+//
+//func notFound(w http.ResponseWriter, r *http.Request) {
+//	w.WriteHeader(http.StatusNotFound)
+//	w.Write([]byte("not found"))
+//}
 
-	fmt.Printf("Starting server for testing HTTP POST...\n")
-	if err := http.ListenAndServe(":8181", nil); err != nil {
-		log.Fatal(err)
+func main() {
+	mux := http.NewServeMux()
+	userH := &userHandler{
+		store: &datastore{
+			m: map[string]user{
+				"1": user{ID: "1", Name: "bob"},
+			},
+			RWMutex: &sync.RWMutex{},
+		},
 	}
+
+	//POST
+	//createEvent = regexp.MustCompile(`^\/create_event[\/]*$`) // ^$ - полное совпадение должно быть
+	//updateEvent = regexp.MustCompile(`^\/update_event[\/]*$`) // \d+ - одна или более цифра доступная позже
+	//deleteEvent = regexp.MustCompile(`^\/delete_event[\/]*$`) // * - жабная функция
+	////GET
+	//getEventForDay   = regexp.MustCompile(`^\/events_for_day[\/]*$`)
+	//getEventForWeek  = regexp.MustCompile(`^\/events_for_week[\/]*$`)
+	//getEventForMonth = regexp.MustCompile(`^\/events_for_month[\/]*$`)
+
+	mux.Handle("/create_event/", userH)
+	mux.Handle("/update_event/", userH)
+
+	http.ListenAndServe("localhost:8080", mux)
 }
