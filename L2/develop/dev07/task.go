0ccad9c6f1b114327d9c1cc6f020ae10d1640f5e
Roman <roman.kocenko.2004@mai.ru>
2022-05-28 16:21:55 +0300

    DEV07: DONE. comments also added

--- L2/develop/dev07/task.go
+++ L2/develop/dev07/task.go
@@ -37,39 +37,40 @@ start := time.Now()
 
 fmt.Printf(“fone after %v”, time.Since(start))
 */
-func or(channels ...<-chan interface{}) <-chan interface{} {
-	doneCh := make(chan interface{})
 
-	switch len(channels) {
-	case 0:
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	doneCh := make(chan interface{}) // создаем канал который передает интерфейсы
+
+	switch len(channels) { // в зависимости от количества каналов делаем:
+	case 0: // если каналов нет
 		return nil
-	case 1:
+	case 1: // если единственный канал
 		return channels[0]
-	default:
-		go func(ch chan interface{}) {
-			defer close(doneCh)
+	default: // если больше чем 1 канал
+		go func(ch chan interface{}) { // запускаем горутину
+			defer close(doneCh) // в конце закроем канал
 			select {
-			case <-channels[0]:
-			case <-or(append(channels[1:])...):
+			case <-channels[0]: //считываем из первого канала
+			case <-or(append(channels[1:])...): // рекурсивно запускаем все кроме первого каналы
 			}
-		}(doneCh)
+		}(doneCh) // передаем done канал
 	}
 	return doneCh
 }
 
 func main() {
 
-	sig := func(after time.Duration) <-chan interface{} {
+	sig := func(after time.Duration) <-chan interface{} { // запускаем функцию по засеканию времени
 		c := make(chan interface{})
-		go func() {
+		go func() { //запускаем горутину для небольшой задержки на несколько секунд
 			defer close(c)
 			time.Sleep(after)
 		}()
 		return c
 	}
-	start := time.Now()
+	start := time.Now() // засекаем начальное время
 	<-or(
-		sig(2*time.Hour),
+		sig(2*time.Hour), // передаем каналы с временем в часах, минутах, секундах
 		sig(5*time.Minute),
 		sig(1*time.Second),
 		sig(1*time.Hour),
