29e4c69f5d59b770a567749f501dac008a137b85
Roman <roman.kocenko.2004@mai.ru>
2022-05-30 10:21:09 +0300

    DEV08: DONE

---
+++ L2/develop/dev08/go.mod
@@ -0,0 +1,3 @@
+module BinFiles
+
+go 1.18
--- L2/develop/dev08/task_test.go
+++ L2/develop/dev08/task_test.go
@@ -1,1 +1,27 @@
 package main
+
+import "testing"
+
+func Test_startBinaryFile(t *testing.T) {
+	type args struct {
+		binPath string
+		data    string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Test first", args{binPath: "cdBin/cd", data: ".."}, "Succesful changing. Now you in /Users/roman/Desktop/Work/WorkRepo/L2/develop."},
+		{"Test second", args{binPath: "pwdBin/pwd", data: ""}, "Now you in /Users/roman/Desktop/Work/WorkRepo/L2/develop/dev08."},
+		{"Test third", args{binPath: "echoBin/echo", data: "Hello"}, "Echo: Hello\n"},
+		{"Test fourth", args{binPath: "killBin/kill", data: ""}, "Process finished successfully(Killed)"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := startBinaryFile(tt.args.binPath, tt.args.data); got != tt.want {
+				t.Errorf("startBinaryFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
